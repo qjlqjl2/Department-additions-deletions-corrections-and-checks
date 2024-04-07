cc74ba6c2d6f53bee1d69c3a906b41ff9e37eb8b
qjlqjl2 <qianjianling676@gmail.com>
2024-04-07 22:52:57 +0800

    test

--- oa/src/qjl/oa/web/action/DeptDelServlet.java
+++ oa/src/qjl/oa/web/action/DeptDelServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import qjl.oa.DBUtil;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
